753611fc32bf2938a918d151468552a33fb6c9a2
Sadesh Warnakulasooriya <sadesh@drjinc.co.jp>
2024-08-21 18:56:43 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define the route to serve the HTML content
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>EC2 Instance Details</title>
+      </head>
+      <body>
+        <h1>EC2 Instance Details</h1>
+        <p>Instance ID: MH-TEST-FE-1A</p>
+        <p>Server Name: Front End Server 01</p>
+        <p>Subnet ID: (Public-subnet-1A)</p>
+      </body>
+    </html>
+  `);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
